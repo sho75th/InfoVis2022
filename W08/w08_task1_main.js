0b115611a8c96c571c53cd7bbebf20bcaa9d99c6
sho75th <shosanblack0212@gmail.com>
2022-05-11 13:48:40 +0900

    Initial commit

---
+++ W08/w08_task1_main.js
@@ -0,0 +1,96 @@
+d3.csv("https://sho75th.github.io/InfoVis2022/W08/w08_task1.csv")
+    .then( data => {
+        data.forEach( d => { d.value = +d.value; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 256,
+            margin: {top:10, right:10, bottom:40, left:40}
+        };
+
+        const BarChart = new BarChart( config, data );
+        BarChart.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
+
+class BarChart {
+
+    constructor( config, data ) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .range( [0, self.inner_width] );
+
+        self.yscale = d3.scaleLinear()
+            .range( [self.inner_height,0] );
+
+        self.xaxis = d3.axisBottom( self.xscale )
+            .ticks(6);
+        
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(6);
+            
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis_group = self.chart.append('g')
+            // .attr('transform', `translate(0, ${self.inner_width})`);
+
+    }
+
+    update() {
+        let self = this;
+
+        const xmin = d3.min( self.data, d => d.x );
+        const xmax = d3.max( self.data, d => d.x );
+        self.xscale.domain( [xmin-20, xmax+20] );
+
+        const ymin = d3.min( self.data, d => d.y );
+        const ymax = d3.max( self.data, d => d.y );
+        self.yscale.domain( [ymin-20, ymax+20] );
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.selectAll("rect")
+            .data(self.data)
+            .enter()
+            .append("rect")
+            .attr("x", 0 )
+            .attr("y", d => self.yscale( d.label ) )
+            .attr("width", d => self.xscale(d.value) )
+            .attr("height", d => self.yscale.bandwidth() );
+
+        self.xaxis_group
+            .call( self.xaxis );
+        self.yaxis_group
+            .call( self.yaxis );
+    }
+}
